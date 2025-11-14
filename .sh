28df6fdd5cedcfde38424a5b50166583b45d6db8
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2025-11-13 22:42:17 -0800

    -

--- .sh
+++ .sh
@@ -8,8 +8,20 @@ flag() {
 tsc
 node dist/pug.js
 sass --no-source-map src/style.scss style.css
-perl -0777 -pe '
-    s/\n{2,}/\n/g;
-    s/(?<=\b0x)0+(?=[0-9A-F]{1,4}\b)//g;
-' src/Main.elm > src/Main.bak.elm
-elm make src/Main.elm --output=elm.js
+mkdir -p src/{bak,tmp}
+ELM=src/Main.elm
+TMP=src/tmp/Main.elm
+BAK=src/bak/Main.elm
+cp $ELM $BAK
+if $(perl -0777 -pe '
+	s/\n{2,}/\n/g;
+    s/(?<=\b0x)0+(?=[0-9A-F]+\b)//g;
+' $ELM > $TMP) && [[ -s $TMP ]]; then
+	mv $TMP $ELM
+	elm make $ELM --output=elm.js
+else
+	cp $BAK $ELM
+	echo "RegEx Clean Error!" >&2
+fi
+rm -rf src/bak
+rm -rf src/tmp
